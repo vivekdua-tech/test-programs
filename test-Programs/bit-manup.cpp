f5a458f081e301d2b2c96051e4fc6ab0c4dc1b90
vivekdua <vivekdua123@yahoo.com>
2018-04-29 17:15:38 -0700

    na

---
+++ test-Programs/bit-manup.cpp
@@ -0,0 +1,69 @@
+//
+//  bit-manup.cpp
+//  test-Programs
+//
+//  Created by Vivek Dua on 4/24/18.
+//  Copyright © 2018 Vivek Dua. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+#define MASK(N) \
+    ~(~0 << N)
+
+// FLEXMASK(X, Y) -> 00001111100000 x and y bit posn have 1's.
+#define FLEXMASK(X, Y) \
+    MASK((X)) ^ MASK((Y - 1))
+
+#define INVERSEFLEXMASK(X, Y) ~FLEXMASK((X), (Y))
+
+//Insert M in N at bit posn X to Y
+#define INSERTVALUE(M, N, X, Y) \
+    (((M) & INVERSEFLEXMASK((X), (Y))) |  (((N) << (Y))))
+
+
+int main ()
+{
+    int m = 0x0a0a0a0a;
+    int n = 0xe;
+    
+    int result = INSERTVALUE(m, n, 8, 5);
+    
+    
+    int mask = FLEXMASK(8, 5);
+    
+    cout << "flex mask is" << hex <<
+    mask << endl;
+    
+    int invmask = INVERSEFLEXMASK(8, 5);
+    
+    
+    cout << "inverse flex mask is" << hex <<
+    invmask << endl;
+    
+    int value1 = m & invmask;
+    
+    cout << "value1 is : " << hex << value1 << endl;
+    
+    int value2 = n << 5;
+    
+    cout << "value2 is : " << hex << value2 << endl;
+    
+    
+    int value3 = value1 | value2;
+    
+    cout << " OR of value1 and value2 is" << hex <<
+    value3 << endl;
+    
+    
+    
+    
+    cout << "Value is" << hex << result << endl;
+    
+}
+
+
+
