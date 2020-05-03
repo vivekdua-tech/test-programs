81b28c56081fc77a57685738bef9b171416a17d5
Vivek Dua <vivekdua1975@gmail.com>
2020-05-03 11:12:57 -0700

    frog jump

--- test-Programs/dynamic-progamming.cpp
+++ test-Programs/dynamic-progamming.cpp
@@ -316,3 +316,44 @@ and str[i] is same as str[j], then we make table[i][j] true. Otherwise, the valu
  
  *************************************************/
 
+
+/***************************************************FROG Jump***************************************************
+ 
+ A frog is crossing a river. The river is divided into x units and at each unit there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
+
+ Given a list of stones' positions (in units) in sorted ascending order, determine if the frog is able to cross the river by landing on the last stone. Initially, the frog is on the first stone and assume the first jump must be 1 unit.
+
+ If the frog's last jump was k units, then its next jump must be either k - 1, k, or k + 1 units. Note that the frog can only jump in the forward direction.
+
+ Note:
+
+ The number of stones is ≥ 2 and is < 1,100.
+ Each stone's position will be a non-negative integer < 231.
+ The first stone's position is always 0.
+ Example 1:
+
+ [0,1,3,5,6,8,12,17]
+
+ There are a total of 8 stones.
+ The first stone at the 0th unit, second stone at the 1st unit,
+ third stone at the 3rd unit, and so on...
+ The last stone at the 17th unit.
+
+ Return true. The frog can jump to the last stone by jumping
+ 1 unit to the 2nd stone, then 2 units to the 3rd stone, then
+ 2 units to the 4th stone, then 3 units to the 6th stone,
+ 4 units to the 7th stone, and 5 units to the 8th stone.
+ 
+ Approach #3 Using Memorization [Accepted]
+ Algorithm
+
+ Another problem with above approaches is that we can make the same function calls coming through different paths e.g. For a given currentIndexcurrentIndex, we can call the recursive function canCrosscanCross with the jumpsizejumpsize, say nn. This nn could be resulting from previous jumpsizejumpsize being n-1n−1,nn or n+1n+1. Thus, many redundant function calls could be made prolonging the running time. This redundancy can be removed by making use of memorization. We make use of a 2-d memomemo array, initialized by -1−1s, to store the result returned from a function call for a particular currentIndexcurrentIndex and jumpsizejumpsize. If the same currentIndexcurrentIndex and jumpsizejumpsize happens is encountered again, we can return the result directly using the memomemo array. This helps to prune the search tree to a great extent.
+ 
+ Approach #5 Using Dynamic Programming[Accepted]
+ Algorithm
+
+ In the DP Approach, we make use of a hashmap mapmap which contains key:valuekey:value pairs such that keykey refers to the position at which a stone is present and valuevalue is a set containing the jumpsizejumpsize which can lead to the current stone position. We start by making a hashmap whose keykeys are all the positions at which a stone is present and the valuevalues are all empty except position 0 whose value contains 0. Then, we start traversing the elements(positions) of the given stone array in sequential order. For the currentPositioncurrentPosition, for every possible jumpsizejumpsize in the valuevalue set, we check if currentPosition + newjumpsizecurrentPosition+newjumpsize exists in the mapmap, where newjumpsizenewjumpsize can be either jumpsize-1jumpsize−1, jumpsizejumpsize, jumpsize+1jumpsize+1. If so, we append the corresponding valuevalue set with newjumpsizenewjumpsize. We continue in the same manner. If at the end, the valuevalue set corresponding to the last position is non-empty, we conclude that reaching the end is possible, otherwise, it isn't.
+
+ *************************************************************************************************************************/
+
+
